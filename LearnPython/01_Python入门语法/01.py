240a5f3508bea8a02212a185a21b57f20174a00c
AaaBinfinity <147581648+AaaBinfinity@users.noreply.github.com>
2024-04-22 20:54:33 +0800

    Add files via upload

---
+++ LearnPython/01_Python入门语法/01.py
@@ -0,0 +1,17 @@
+print(888)         #888
+print("8-2")        #8-2
+print('8-2')         #8-2
+print(8-2)          #6
+# print('8'-'6')    字符串不能相减
+print("caobin ")
+print(True)
+print(False)
+
+
+
+8
+1.232
+"徐静"
+'5'
+True
+False

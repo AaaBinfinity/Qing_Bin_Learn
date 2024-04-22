4bf583c6d6333b5676890cf93c5cdf60fac09575
AaaBinfinity <147581648+AaaBinfinity@users.noreply.github.com>
2024-04-22 22:14:57 +0800

    08_字符串扩展_01字符串的定义.py

--- LearnPython/01_Python入门语法/08_字符串扩展_01字符串的定义.py
+++ LearnPython/01_Python入门语法/08_字符串扩展_01字符串的定义.py
@@ -15,7 +15,7 @@ print(name)
 # 嵌套的引号不能跟定义用的引号相同
 name = '"binfinity"'        #定义的效用被单引号占用了
 print(name)
-name = "'binfinity' "       #定义的效用被单引号占用了
+name = "'binfinity' "       #定义的效用被单引号占用了，所以可以打印出引号
 print(name)
 # 必须相同的话可以使用（\）来将被嵌套的引号解除效用，变成普通字符
 name = "\"binfinity\""

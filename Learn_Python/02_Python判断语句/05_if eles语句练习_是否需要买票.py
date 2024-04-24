310c3e0e7e4b875f5f40268a59a3fbec11f1e683
Your Name <your_email@example.com>
2024-04-25 06:30:00 +0800

    Binfinity Add

---
+++ Learn_Python/02_Python判断语句/05_if eles语句练习_是否需要买票.py
@@ -0,0 +1,11 @@
+# 欢迎程序
+print("欢迎来到游乐城")
+# 通过用户输入获取用户身高
+height = int(input("请输入您的身高："))
+# 判断身高
+if height >= 120:
+    print("您的身高超过120cm，请购票")  # 身高超过时执行
+else:
+    print("您的身高未超过120cm，可以免费游玩")  # 身高没超过时执行
+# 祝福程序
+print("Have A Good Time!")

18817a3cf961a30c33e25ad1d96b34c3b07d9c2f
Your Name <your_email@example.com>
2024-04-24 22:25:20 +0800

    Binfinity Add

--- Learn_Python/02_Python判断语句/01_布尔类型与比较运算符.py
+++ Learn_Python/02_Python判断语句/01_布尔类型与比较运算符.py
@@ -23,7 +23,35 @@ Python逻辑判断：
 
 # 定义变量用来储存布尔数据类型：变量名称 = 布尔类型字面量
 Love = True
-print(type(Love))       # <class 'bool'>
+print(type(Love))  # <class 'bool'>
 
-result = 10 > 9
-print(f"10 > 9的结果是：{result}，result的类型为{type(result)}")
+# 大于号就是一个比较运算符
+result = 10 > 9  # 判断10和9哪个更大
+print(f"10 > 9的结果是：{result}，result的类型为{type(result)}")  # 10 > 9的结果是：True，result的类型为<class 'bool'>
+
+# "=="是一个比较运算符，判断左右是否相等
+name_result = "QingQing" == "Binfinity"
+print(
+    "\"QingQing\"和\"Binfinity\"两个字符串是否相同的比较结果是"   # 这里的“+”(拼接字符串)可以省略
+    f"{name_result}，类型是{type(name_result)}")  # False，类型是<class 'bool'>
+
+
+"""
+
+比较运算符
+
+    运算符                                      描述                                             示例
+    
+     ==                         判断内容是否相等，满足为True，不满足为False                 a=3,b=3，则（a == b) 为 True
+
+     !=                       判断内容是否不相等，满足为True，不满足为False                 a=1,b=3，则（a!=b）为True
+
+      <                              判断运算符左侧内容是否小于右侧                         a=3,b=7，则（a < b）为True
+
+      >                             判断运算符左侧内容是否大于右侧                          a=7,b=3，则（a> b）为True
+
+     >=                             判断运算符左侧内容是否大于等于右侧                       a=3,b=3，则（a >= b）为True
+
+      <=                            判断运算符左侧内容是否小于等于右侧                       a=3,b=3，则（a <= b) 为True
+
+"""

8c9be303e2d30bfd59312f572616a07591d38655
Your Name <your_email@example.com>
2024-04-24 21:04:45 +0800

    Binfinity Add

--- Learn_Python/01_Python入门语法/01.py
+++ Learn_Python/01_Python入门语法/01.py
@@ -1,13 +1,11 @@
-print(888)         #888
-print("8-2")        #8-2
-print('8-2')         #8-2
-print(8-2)          #6
+print(888)  # 888
+print("8-2")  # 8-2
+print('8-2')  # 8-2
+print(8 - 2)  # 6
 # print('8'-'6')    字符串不能相减
 print("caobin ")
 print(True)
 print(False)
-
-
 
 8
 1.232

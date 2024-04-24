9285e755a7fa228403da53b4cb7009f7d09f571a
Your Name <your_email@example.com>
2024-04-24 21:32:44 +0800

    Binfinity Add

--- Learn_Python/01_Python入门语法/10_数据输入.py
+++ Learn_Python/01_Python入门语法/10_数据输入.py
@@ -7,16 +7,16 @@ input语句（函数）的使用
 
 # 使用input（）语句可以从键盘获取输入
 # 使用一个变量接收（存储）input语句获取的键盘输入数据即可
-print("请告诉我你是谁")   # 提示语句
+print("请告诉我你是谁")  # 提示语句
 name = input()
 print("我知道了，你是%s" % name)
 
 # 输出"请告诉我你是谁“的print语句其实是多余的
 # 可以使用：input(提示信息)，在使用者输入内容前,显示提示内容
-name = input("请告诉我你是谁")     # 提示信息可以直接写在input的括号里
+name = input("请告诉我你是谁")  # 提示信息可以直接写在input的括号里
 print("我知道了，你是%s" % name)
 
-# 无论你写入的是什么样的数据，input语句通通都把它当做字符串来看待
+# 无论写入的是什么样的数据，input语句通通都把它当做字符串来看待
 num = input("李绮卿的学号是什么？")
 print("李绮卿学号的类型是：", type(num))
 
@@ -24,5 +24,6 @@ print("李绮卿学号的类型是：", type(num))
 num = input("李绮卿学号的类型是")
 num = int(num)
 print("李绮卿学号的类型是：", type(num))
+
 print("I Love You")
 # print("I Love You")

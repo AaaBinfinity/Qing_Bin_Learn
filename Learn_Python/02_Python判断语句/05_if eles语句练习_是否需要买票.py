bc34dcc68653750d54a294fdcb6569d15aa9ac69
Qing <m13314133031@163.com>
2024-04-26 11:31:30 +0800

    Qing add

--- Learn_Python/02_Python判断语句/05_if eles语句练习_是否需要买票.py
+++ Learn_Python/02_Python判断语句/05_if eles语句练习_是否需要买票.py
@@ -9,3 +9,26 @@ else:
     print("您的身高未超过120cm，可以免费游玩")  # 身高没超过时执行
 # 祝福程序
 print("Have A Good Time!")
+
+# 卿卿版
+print("欢迎来到动物园")
+height = int(input("请输入你的身高"))
+if height >= 120:
+    print("您的身高超出120cm，游玩需要购票10元")
+else:
+    print("您可以免费游玩")
+print("祝您游玩愉快！")
+
+
+
+
+
+
+
+
+
+
+
+
+
+

43a0bfb6354e533c60e44b77cff5adffb13e44b0
Your Name <your_email@example.com>
2024-04-27 19:51:36 +0800

    Binfinity Add

--- Learn_Python/02_Python判断语句/06_if elif else语句.py
+++ Learn_Python/02_Python判断语句/06_if elif else语句.py
@@ -24,9 +24,34 @@ elif 条件3:
      满足条件要做的行为3
      满足条件要做的行为4
 else:
-     满足条件要做的行为2
-     满足条件要做的行为3
-     满足条件要做的行为4
-
+     条件都不满足时要做的行为1
+     条件都不满足时要做的行为2
+     条件都不满足时要做的行为3
 
 """
+
+print("欢迎来到动物园")
+height = int(input("请输入你的身高："))
+vip_level = int(input("请输入你的vip等级（1~5）:"))
+if height < 120:
+    print("免费游玩")
+elif vip_level > 3:
+    print("免费游玩")
+else:
+    print("滚去交钱")
+# print("代码执行完成")
+
+
+# pro
+print("欢迎来到动物园")
+
+height = int(input("请输入你的身高："))
+
+if height < 120:
+    print("免费游玩")
+else:
+    vip_level = int(input("请输入你的vip等级（1~5）:"))
+    if vip_level > 3:
+        print("免费游玩")
+    else:
+        print("滚去交钱")

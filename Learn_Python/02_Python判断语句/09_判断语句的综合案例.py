394f9f88066420ff787d3844f35483b1dbbda5ed
QingQing <m13314133031@163.com>
2024-05-03 14:02:07 +0800

    QingQing Add

--- Learn_Python/02_Python判断语句/09_判断语句的综合案例.py
+++ Learn_Python/02_Python判断语句/09_判断语句的综合案例.py
@@ -13,26 +13,26 @@ print("Hi Player~")
 import random
 num = random.randint(1, 10)
 print("让我们一起猜数字吧~")
-guess1 = int(input("你猜的数字是"))
+guess1 = int(input("你猜的数字是:"))
 if guess1 != num:
     if guess1 > num:
-        print("太大啦，再试一次吧")
+        print("太大啦，再试一次吧~")
     elif guess1 < num:
-        print("太小啦，再试一次吧")
+        print("太小啦，再试一次吧~")
 
-    guess2 = int(input("你猜的数字是"))
+    guess2 = int(input("你猜的数字是~"))
     if guess2 != num:
         if guess2 > num:
-            print("太大啦，再再试一次吧")
+            print("太大啦，再再试一次吧~")
         elif guess2 < num:
-            print("太小啦，再再试一次吧")
+            print("太小啦，再再试一次吧~")
 
-        guess3 = int(input("你猜的数字是"))
+        guess3 = int(input("你猜的数字是~"))
         if guess3 != num:
             if guess3 > num:
-                print("太大啦，别试了笨蛋")
+                print("太大啦，别试了笨蛋~")
             elif guess3 < num:
-                print("太小啦，别试了笨蛋")
+                print("太小啦，别试了笨蛋~")
 
         else:
             print("猜对啦，棒棒哒~")

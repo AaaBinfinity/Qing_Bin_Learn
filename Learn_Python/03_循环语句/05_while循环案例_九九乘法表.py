2a35f639f4de0e600f68d6558146c0e48041c59a
QingQing <m13314133031@163.com>
2024-05-10 09:28:25 +0800

    QingQing Add

---
+++ Learn_Python/03_循环语句/05_while循环案例_九九乘法表.py
@@ -0,0 +1,36 @@
+# 2024年5月9日18:35:14
+# print输入不换行
+"""
+print("Hello")
+print("World")
+
+结果：
+Hello
+World
+在print语句中，加上end=''即可输出不换行
+
+print("Hello",end='')
+print("World",end='')
+结果：
+HelloWorld
+"""
+
+"""
+# 制表符\t
+效果等同于tab键，可以让我们的多行字符串进行对齐
+print("hello\tworld")
+print("Qing\tBin")
+
+结果：
+hello world
+Qing  Bin
+"""
+
+# 练习案例_打印九九乘法表
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        print(f"{j} * {i} = ", i * j, end='')
+        j += 1
+    i = i + 1

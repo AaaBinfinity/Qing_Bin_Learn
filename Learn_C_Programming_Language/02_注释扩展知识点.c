f28c306f86b0a10039e3d560648b8acaef8b5334
QingQing <m13314133031@163.com>
2024-05-28 16:34:16 +0800

    QingQing Add

---
+++ Learn_C_Programming_Language/02_注释扩展知识点.c
@@ -0,0 +1,16 @@
+//注释快捷键：ctrl+k,ctrl+c,ctrl+shift+/
+//取消快捷键：Ctrl+k,ctrl+u
+
+/*常见代码快捷键;
+格式化代码  Ctrl + k，F
+向下插入一行 Ctrl+shift+回车
+全部变为大写 CTRL+shift+u
+全部变为小写 Ctrl+u
+删除当前行 Ctrl+shift+L
+*/
+
+//注释擦除：注释的内容，不参与运行
+//注释的嵌套：注释最好不要嵌套
+
+
+
---
+++ Learn_C_Programming_Language/06_输出常量.c
@@ -0,0 +1,23 @@
+/*
+输出常量
+	printf（参数1，参数2）输出语句
+
+参数1：必填，输出内容的最终样式以字符串的形式体现
+参数2：选填，填补的内容
+*/
+
+/*
+格式控制符	说明	单词
+整型		%d		decimal
+实型		%f		floating-point
+字符		%c      character
+字符串		%s		string
+*/
+
+//输出一个整数
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%d", 12);
+//	return 0;
